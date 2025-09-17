60aa63967f4f0e6dfdfc51ec05af07a912157571
Sanjai <sanjairajan1694@gmail.com>
2025-09-17 17:28:46 +0530

    Add files via upload

--- SSR-Vanga-Samaikalam-LEts-GO-Cookin--VAI-Team-main/src/components/Footer.jsx
+++ SSR-Vanga-Samaikalam-LEts-GO-Cookin--VAI-Team-main/src/components/Footer.jsx
@@ -9,33 +9,33 @@ const Footer = () => {
   return (
     <div className='footer'>
 
-        <h3>SB Recipess...</h3>
+        <h3 > <i> 🧑‍🍳Cookin Guide🍴</i> </h3>
 
         <div className="footer-options">
           <ul>
-            <li  onClick={()=>navigate(`/`)}>Home</li>
-            <li  onClick={()=>navigate(`/category/Chicken`)}>Chicken</li>
-            <li  onClick={()=>navigate(`/category/Breakfast`)}>Breakfast</li>
+            <li  onClick={()=>navigate(`/`)}>Go Home🏠</li>
+            <li  onClick={()=>navigate(`/category/Chicken`)}>Chicken🍗🐔</li>
+            <li  onClick={()=>navigate(`/category/Breakfast`)}>Breakfast🥪🥞</li>
           </ul>
           <ul>
-            <li  onClick={()=>navigate(`/category/Dessert`)}>Dessert</li>
-            <li  onClick={()=>navigate(`/category/Goat`)}>Goat</li>
-            <li  onClick={()=>navigate(`/category/Lamb`)}>Lamb</li>
+            <li  onClick={()=>navigate(`/category/Dessert`)}>Dessert🍦/Sweet For Tounge🍧</li>
+            <li  onClick={()=>navigate(`/category/Goat`)}>Goat🐐/Mutton🍖</li>
+            <li  onClick={()=>navigate(`/category/Lamb`)}>Lamb🥩</li>
           </ul>
           <ul>
-            <li  onClick={()=>navigate(`/category/Pasta`)}>Pasta</li>
-            <li  onClick={()=>navigate(`/category/Seafood`)}>Seafood</li>
-            <li  onClick={()=>navigate(`/category/Starter`)}>Starter</li>
+            <li  onClick={()=>navigate(`/category/Pasta`)}>Noodles🍜/Carbs🍝</li>
+            <li  onClick={()=>navigate(`/category/Seafood`)}>Fresh From Waters ?🦈🐟🍤</li>
+            <li  onClick={()=>navigate(`/category/Starter`)}>KickStarters🍟</li>
           </ul>
           <ul>
-            <li  onClick={()=>navigate(`/category/Vegan`)}>Vegan</li>
-            <li  onClick={()=>navigate(`/category/Side`)}>Side</li>
+            <li  onClick={()=>navigate(`/category/Vegan`)}>Only Greens Are WE?🥬🥗🍏</li>
+            <li  onClick={()=>navigate(`/category/Side`)}>Side/Dips</li>
             <li  onClick={()=>navigate(`/category/Miscellaneous`)}>Miscellaneous</li>
           </ul>
         </div>
 
         <hr />
-        <p>SB Recipiess - &copy; 2023 - All Rights Reserved</p>
+        <p>  🧑‍🍳Cookin Guide🍴 - &copy; 2025 - All Rights Reserved</p>
     </div>
   )
 }
--- SSR-Vanga-Samaikalam-LEts-GO-Cookin--VAI-Team-main/src/components/Hero.jsx
+++ SSR-Vanga-Samaikalam-LEts-GO-Cookin--VAI-Team-main/src/components/Hero.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import '../styles/Hero.css'
-import heroImg1 from '../images/hero-img1.png'
-import heroImg2 from '../images/hero-img2.png'
-import heroImg3 from '../images/hero-img3.png'
-import heroImg4 from '../images/hero-img4.png'
+import heroImg1 from '../images/Biriyani.jpg'
+import heroImg2 from '../images/Pastha.jpg'
+import heroImg3 from '../images/seafood.jpg'
+import heroImg4 from '../images/Greens.jpg'
 
 const Hero = () => {
   return (
     <div className='hero-container'>
-        <div className="hero-text">
-              <div className="hero-line" />
-              <h1>Discover delicious recipes for every occasion..</h1>
-              <p>Ready to tantalize your taste buds? Dive into our endless recipe library and find your next food adventure.</p>
-              <a href="#recipies"><button>Recipies</button></a>
-        </div>
-        <div className="hero-images">
+      <div className="hero-images">
             <span className='span1'>
               <img src={heroImg2} alt="" />
               <img src={heroImg4} alt="" />
@@ -24,6 +18,13 @@ const Hero = () => {
               <img src={heroImg1} alt="" />
             </span>
         </div>
+        <div className="hero-text">
+              <div className="hero-line" />
+              <h1>Discover delicious recipes for every occasion..</h1>
+              <p>Ready to Revitalise your taste buds? Dive into our endless recipe library and find your next food adventure.</p>
+              <a href="#recipies"><button>Guide To The Vault OF Recipies</button></a>
+        </div>
+        
     </div>
   )
 }
